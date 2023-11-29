2a090385e2d7e58371960034c1d00344c138cb9d
David Konsumer <konsumer@jetboystudio.com>
2023-11-29 02:01:41 -0800

    more basic demo

--- test.js
+++ test.js
@@ -13,13 +13,13 @@ import {
 } from './build/raylibqjs.so'
 
 InitWindow(800, 450, 'raylib [core] example - basic window')
-SetTargetFPS(60)
+// SetTargetFPS(60)
 
 while (!WindowShouldClose()) {
   BeginDrawing()
   ClearBackground(RAYWHITE)
   DrawFPS(10, 10)
-  DrawText('Congrats! You created your first window!', 190, 200, 20, LIGHTGRAY)
+  DrawText('Congrats! You created your first raylibqjs window!', 140, 200, 20, LIGHTGRAY)
   EndDrawing()
 }
 
